dc1b9c587d9a9a429feae13788dd101ca35ab469
ethanscho <>
2020-08-05 13:47:36 +0900

    Add squat count controller

---
+++ unity/Assets/PoseNet/SquatController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TensorFlowLite;
+
+public class SquatController : MonoBehaviour
+{
+	public PoseNetSample poseNetSample;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var results = poseNetSample.results;
+
+        foreach(PoseNet.Result result in results)
+        {
+            if (result.confidence > 0.5f)
+            {
+                Debug.Log(result.part);
+                Debug.Log(result.x);
+                Debug.Log(result.y);
+            }
+        }
+	}
+}
