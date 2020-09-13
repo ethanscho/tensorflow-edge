f41b8e9998c43dbfc75ac84417558f241344be3e
ethanscho <>
2020-09-13 15:00:03 +0900

    Add calendar framework

---
+++ unity/Assets/Scripts/Calendar/CalendarController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalendarController : MonoBehaviour
+{
+    public Text monthYearText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DateTime now = System.DateTime.Now;
+        print(now);
+        int year = now.Year;
+        int month = now.Month;
+        int days = DateTime.DaysInMonth(year, month);
+
+        monthYearText.text = year.ToString() + "." + now.Month.ToString();
+
+        for (int i = 0; i < days; i++)
+        {
+            DateTime date = new DateTime(year, month, i + 1);
+            DayOfWeek day = date.DayOfWeek;
+
+        }
+    }
+}
